f977f13349dfa34d9bd44ccbb7619e7bc22eff78
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2018-06-17 15:05:32 +0200

    Update distro list
    • Added aio, obarun, solaris, stella
    • Updated 4mlinux, alt, antergos, archbang, archlabs, archman, arco, austrumi,
      berry, blankon, bluestar, bodhi, centos, container, cucumber, devuan,
      dragonflybsd, emmabuntus, extix, gecko, gparted, greenie, karoshi, kdeneon,
      knoppix, kwort, linuxfx, liquidlemur, lite, liveraizo, lxle, manjaro, mint,
      namib, nethserver, nitrux, nutyx, omarine, opensuse, parabola, pclinuxos,
      pinguy, plamo, pld, proxmox, q4os, robolinux, scientific, siduction, sparky,
      swagarch, tails, ubuntu, univention, untangle
    • Removed backslash, korora, revengeos, tanglu

--- lib/System/Info.pm
+++ lib/System/Info.pm
@@ -3,7 +3,7 @@ package System::Info;
 use strict;
 use warnings;
 
-our $VERSION = "0.058";
+our $VERSION = "0.059";
 
 use base "Exporter";
 our @EXPORT_OK = qw( &sysinfo &sysinfo_hash &si_uname );
