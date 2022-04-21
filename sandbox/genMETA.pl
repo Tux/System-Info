1d725ea211e3a677dab901a0670db4ad23a90d49
H.Merijn Brand - Tux <linux@tux.freedom.nl>
2022-04-21 16:01:17 +0200

    Versions

--- sandbox/genMETA.pl
+++ sandbox/genMETA.pl
@@ -99,7 +99,7 @@ test_requires:
     Test::More:          0.88
     Test::Warnings:      0
 test_recommends:
-    Test::More:          1.302188
+    Test::More:          1.302190
 resources:
     license:             http://dev.perl.org/licenses/
     repository:          https://github.com/Tux/System-Info
